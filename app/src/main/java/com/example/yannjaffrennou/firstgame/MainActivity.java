e3d3b46c750b7d23b422d34cfb4ee18a63c434a1
Yann Jaffrennou <yann.jaffrennou86@gmail.com>
2019-03-09 17:11:25 +0100

    Initial commit

---
+++ app/src/main/java/com/example/yannjaffrennou/firstgame/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yannjaffrennou.firstgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // On cré un objet "GameView" qui est le code principal du jeu
+        gameView=new GameView(this);
+
+        // et on l'affiche.
+        setContentView(gameView);
+    }
+
+} // class MainActivity
